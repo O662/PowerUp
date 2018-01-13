3b9e35abd0a9da683617f297873e176a6ce3564e
jkmacy <joyousjake.jm@gmail.com>
2018-01-13 17:07:29 -0600

    Climber, Arm, Glove now proper subsystem classes.

--- src/org/usfirst/frc/team3335/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team3335/robot/subsystems/Climber.java
@@ -1,4 +1,15 @@
 package org.usfirst.frc.team3335.robot.subsystems;
 
-public class Climber {
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Climber extends Subsystem implements LoggableSubsystem {
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    @Override
+    public void log() {
+
+    }
 }
--- src/org/usfirst/frc/team3335/robot/subsystems/Glove.java
+++ src/org/usfirst/frc/team3335/robot/subsystems/Glove.java
@@ -1,4 +1,15 @@
 package org.usfirst.frc.team3335.robot.subsystems;
 
-public class Glove {
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Glove extends Subsystem implements LoggableSubsystem {
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    @Override
+    public void log() {
+
+    }
 }
